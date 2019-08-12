06aec1dff65c6f7b696669dc850663e288c41468
FarahMint <zfarah.el@gmail.com>
2019-08-12 21:37:14 +0100

    remove key

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
     let selectedSymbol;
     let volidxMarket;
     
-    const ws =  new WebSocket(`wss://ws.binaryws.com/websockets/v3?app_id=1089`);
+    const ws =  new WebSocket(`wss://ws.binaryws.com/websockets/v3?app_id=${process.env.WEB_SOCKET_KEY}`);
     
     const init=()=> {
         actionWebSocket();
